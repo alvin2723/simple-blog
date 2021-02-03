b3ccb5a5c6abd660748bee3673b6cabd0bb0e8df
alvin2723 <alvinjulian87@gmail.com>
2021-02-03 18:55:12 +0700

    update favicon

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
+<link rel="icon" href="http://example.com/favicon.png">
 
 <title>Simple Blog</title>
 
